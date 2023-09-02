32d7193465e8901a8ca99be3a9da856880d9ffab
maykmayk <m.devita@ied.edu>
2023-09-02 13:19:50 +0200

    -

--- js/work-details.js
+++ js/work-details.js
@@ -5,59 +5,42 @@ function getUrlParameter(name) {
 
 function loadWorkDetails(workId) {
 	const workDetails = {
-		oggetto: {
-			name: "Oggetto",
-			subName: "Fotografia istantanea, 46x62mm ognuna, 2021",
-            text: "Al giorno d’oggi ha luogo la scomparsa di ciò che ci sta di fronte: la totale assenza di un “essere contro” non rappresenta una condizione ideale, poichè senza questo scontro si ricade dolorosamente su sé stessi. L’essere di fronte avviene come incontro con il totalmente Altro, ma il sovraccarico narcisistico rischia di renderci sordi e ciechi di fronte all’Altro. Ma l’Altro mina la presenza a Sé. Apre una profonda crepa nel soggetto, attraverso la quale il totalmente Altro irrompe nel Sè; solo una debolezza rende ricettivi alla voce dell’Altro. È nell’apertura, nel rendere fragili e vulnerabili le fotografie che lì si mostra la loro vera essenza; ciò che le compone, elemento per elemento, rivelando una bellezza non visibile nella totalità della loro apparenza. Un Oggetto fisico con cui avere un’altrettanto fisico riscontro.<br><br><br>Federica Zandomeneghi",
+		borseMare: {
+			title: "Borse da Mare",
+			headImg: "../asset/images/borse/_MG_2688.jpg",
+            text: "Al giorno d’oggi ha luogo la scomparsa di ciò che ci sta di fronte: la totale assenza di un “essere contro” non rappresenta una condizione ideale, poichè senza questo scontro si ricade dolorosamente su sé stessi.",
             images: [
-                "./asset/images/oggetto/0.jpg",
-                "./asset/images/oggetto/1.jpg",
-                "./asset/images/oggetto/2.jpg",
-                "./asset/images/oggetto/3.jpg",
-                "./asset/images/oggetto/4.jpg",
-                "./asset/images/oggetto/5.jpg",
-                "./asset/images/oggetto/6.jpg",
-                "./asset/images/oggetto/7.jpg",
-                "./asset/images/oggetto/8.jpg",
-                "./asset/images/oggetto/9.jpg",
+                "../asset/images/borse/_MG_2688.jpg",
+                "../asset/images/borse/_MG_2710.jpg",
+                "../asset/images/borse/_MG_2725.jpg",
+                "../asset/images/borse/_MG_2754.jpg",
+                "../asset/images/borse/_MG_2765.jpg",
+                "../asset/images/borse/_MG_2775.jpg",
+                "../asset/images/borse/_MG_2780.jpg",
+                "../asset/images/borse/_MG_2790.jpg",
+                "../asset/images/borse/_MG_2804.jpg",
+                "../asset/images/borse/_MG_2817 copia.jpg",
+                "../asset/images/borse/_MG_2817.jpg",
+                "../asset/images/borse/_MG_2834.jpg",
             ], 
         },
-		puntoDiDomanda: {
-			name: "Punto di Domanda",
-			subName: "Fotografia istantanea, 20x30cm ognuna, 2022",
-            text: "In questo lavoro non si vede nessuna immagine, non c’è nessuna rappresentazione, qualcosa che esiste al di fuori della fotografia e che la fotografia rappresenta: potremmo dire che queste stampe non rappresentano nulla nonostante un soggetto ci sia, nonostante e proprio perchè nascosto. Nella misura in cui non è rappresentato nulla, queste rielaborazioni fotografiche mostrano semplicemente sé stesse. Cosa è presente? L’indagine che ho affrontato mi ha permesso di arrivare a creare un limite aperto, una ferita che nello stesso tempo in cui assume il compito di apertura e rivelatrice nasconde, protegge, obbliga ad un personale lavoro di scoperta e di domanda. Il titolo nasce proprio da questo: venti stampe che attraggono, seducono la vista, ma non basta. Il mio desiderio è stato quello di creare un’opportunità di silenzio. Un punto di riflessione, di indagine personale da cui possa fiorire il sé. Un punto di domanda.<br><br><br>Federica Zandomeneghi",
+		zeintaAdMer: {
+			title: "Zeinta Ad Mer",
+			headImg: "../asset/images/borse/_MG_2688.jpg",
+            text: "Al giorno d’oggi ha luogo la scomparsa di ciò che ci sta di fronte: la totale assenza di un “essere contro” non rappresenta una condizione ideale, poichè senza questo scontro si ricade dolorosamente su sé stessi.",
             images: [
-                "./asset/images/puntoDiDomanda/12.jpg",
-                "./asset/images/puntoDiDomanda/12.jpg",
-                "./asset/images/puntoDiDomanda/13.jpg",
-                "./asset/images/puntoDiDomanda/14.jpg",
-                "./asset/images/puntoDiDomanda/10.jpg",
-            ], 
-        },
-		sindone: {
-			name: "Sindone",
-			subName: "Chimigramma stampato su raso, 115x160cm, 2022-23",
-            text: "“Perché non possiamo restare chiusi in noi stessi? Perché perseguiamo l’espressione e la forma, cercando di svuotarci di ogni contenuto e di disciplinare un processo caotico e ribelle? (…) Essere pieni di sé - non nel senso dell’orgoglio, ma della ricchezza -, essere travagliati da un’infinità interiore e da una estrema tensione significa vivere con una tale intensità da sentirsi morire di vita (…) ma il crollo nascerà allora da un eccesso di pienezza”.<br> E. Cioran, Al culmine della disperazione, 1934 <br><br>I chimigrammi sono una continua interferenza tra intenzione e casualità. Non solo durante, ma anche nel loro successivo sviluppo non sono padrona dell’esecuzione: sono oggetti che prendono effettivamente vita, divengono una vita ferita, e se non fermati chimicamente, la fase di sviluppo non ha un termine, bensì è continua; i fotogrammi assumono un colore progressivamente più scuro, fino al nero, fino a raggiungere l’oscurazione totale. Morte. Nulla di più umano, per quanto sia la cosa che ci dimentichiamo più spesso. Così la fotografia diventa un atto generativo, e non più rappresentativo o imitativo: rafforza l’idea di essa come oggetto piuttosto che di sola immagine. Lo scorrere delle gocce dei bagni di sviluppo sulla carta diventano radici, vasi sanguigni, elementi vitali e di vita. I toni di colore non sono molti, sono quelli della cancrena, una carne lacerata dalla vita - e non dalla morte - che giorno dopo giorno giunge al suo termine. Accanto a queste, un momento fermato, un istante di vita colto nella sua pienezza e immortalato: letteralmente, il suo carattere di mortalità viene annullato, consegnando questo frammento all’eternità.<br><br><br>Federica Zandomeneghi",
-            images: [
-                "./asset/images/sindone/15.png",
-                "./asset/images/sindone/15.png",
-                "./asset/images/sindone/15.png",
-            ], 
-        },
-		autoritratto: {
-			name: "Autoritratto",
-			subName: "",
-            text: "Prima ipotesi<br>Olio su tela di canapa preparata con garze sterili, 40x50 cm, 2023<br>Seconda ipotesi<br>Olio su tela preparata nera, 20x55 cm, 2023<br>Terza ipotesi<br>Olio su tela di canapa non preparata con inserti di garze, 50x50 cm, 2023<br>Ipotesi finale<br>Olio su tela di canapa preparata con garze sterili e inserto di tela ghinea, 50x70 cm, 2023",
-            images: [
-                "",
-            ], 
-        },
-		senzaTitolo: {
-			name: "Senza Titolo",
-			subName: "(Rahmanos) - Disegno su gesso, 30x30cm e 20x35cm, 2022",
-            text: "Questa serie nasce dalla personale ricerca del mio segno. Segno che su carta, da armonioso è diventato sempre più duro, rapido e sintetico fino a essere quasi tagliente; sfruttando questa sua caratteristica ho trasferito l’indagine su un altro tipo di supporto, certamente non tradizionale nel campo del disegno. Lasciandomi guidare dal segno stesso, sono arrivata a toccare una tematica che non mi è mai stata chiara: la Compassione, nei miei confronti e nei confronti dell’Altro. Rahmanos, la parola ebraica per “Compassione”: ciò che più mi affascinato di essa è il fatto che la sua radice indica l’esperienza provata dalla madre che sente il bambino muoversi nel grembo. La scelta del soggetto è stata dunque frutto di entrambi i fattori, oltre alla lunga analisi del segno e della pennellata utilizzati da Mantegna, Bellini, Grünewald e Michelangelo (unico ingrandimento scultoreo).",
-            images: [
-                "",
+                "../asset/images/borse/_MG_2688.jpg",
+                "../asset/images/borse/_MG_2710.jpg",
+                "../asset/images/borse/_MG_2725.jpg",
+                "../asset/images/borse/_MG_2754.jpg",
+                "../asset/images/borse/_MG_2765.jpg",
+                "../asset/images/borse/_MG_2775.jpg",
+                "../asset/images/borse/_MG_2780.jpg",
+                "../asset/images/borse/_MG_2790.jpg",
+                "../asset/images/borse/_MG_2804.jpg",
+                "../asset/images/borse/_MG_2817 copia.jpg",
+                "../asset/images/borse/_MG_2817.jpg",
+                "../asset/images/borse/_MG_2834.jpg",
             ], 
         },
     }
@@ -67,27 +50,31 @@ function loadWorkDetails(workId) {
 		const workDetailsContainer = document.getElementById("workDetailsContainer");
 
         let imagesHtml = '';
-
         // Genera il markup HTML per le immagini
         if (work.images && work.images.length > 0) {
             for (let i = 0; i < work.images.length; i++) {
                 imagesHtml += `
-                <div class="workCont ratio ratio-1x1">
-                    <img src="${work.images[i]}" class="imgFit">
-                </div>`;
+                    <div class="col-6 workGalleryCont"><img src="${work.images[i]}" class="imgFit"></div>
+                `;
             }
         }
-
 		workDetailsContainer.innerHTML = `
-        <div class="pb-1 w-100 text-center instrumentalSerif workTitle">${work.name}</div>
-        <div class="w-100 text-center fs-6 mb-5">${work.subName}</div>
-        <div class="d-flex row mt-5">
-            <div class="col-12 col-md-6 d-flex flex-column gap-3">
-                ${imagesHtml}
+        <div class="workHeaderImgCont">
+            <img src="${work.headImg}"class="imgFit">
+        </div>
+        <div class="row d-flex mt-4">
+            <div class="col-4">
+                <div class="title">${work.title}</div>
             </div>
-            <div class="mt-4 mt-md-0 col-12 col-md-6 fs-5 sticky-column">
-                ${work.text}
+            <div class="col-2"></div>
+            <div class="col-6">
+                <div class="ibmMonoSmall">
+                    ${work.text}
+                </div>
             </div>
+        </div>
+        <div class="row g-2 mt-3">
+            ${imagesHtml}
         </div>
         `
 	}
